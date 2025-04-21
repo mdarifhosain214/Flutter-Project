331ea5cbc83a7d7eeb28a013f2253443ecc64db8
arif <mdarifhosain214@gmail.com>
2025-04-21 17:00:14 +0600

    added font manager

---
+++ flutter_mvvm_clean_architectur/lib/presentation/font_manager.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FontConstants {
+  static const String fontFamily = "Montserrat";
+}
+
+class FontWeightManager {
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+}
+
+class FontSize {
+  static double s12 = 12.0;
+  static double s14 = 14.0;
+  static double s16 = 16.0;
+  static double s17 = 17.0;
+  static double s18 = 18.0;
+  static double s20 = 02.0;
+}
