a47ffc0449c8ac1d135120bb7d5a086c6bde65ed
arif <mdarifhosain214@gmail.com>
2025-04-22 10:21:15 +0600

    added font ,style and value manager

--- flutter_mvvm_clean_architectur/lib/presentation/font_manager.dart
+++ flutter_mvvm_clean_architectur/lib/presentation/font_manager.dart
@@ -13,10 +13,10 @@ class FontWeightManager {
 }
 
 class FontSize {
-  static double s12 = 12.0;
-  static double s14 = 14.0;
-  static double s16 = 16.0;
-  static double s17 = 17.0;
-  static double s18 = 18.0;
-  static double s20 = 02.0;
+  static const double s12 = 12.0;
+  static const double s14 = 14.0;
+  static const double s16 = 16.0;
+  static const double s17 = 17.0;
+  static const double s18 = 18.0;
+  static const double s20 = 02.0;
 }
