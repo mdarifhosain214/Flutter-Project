5e1a56ed5dee3c49aceb06c39aa70424093af926
arif <mdarifhosain214@gmail.com>
2025-04-23 19:34:46 +0600

    splash screen done

--- flutter_mvvm_clean_architectur/lib/presentation/splash/splash.dart
+++ flutter_mvvm_clean_architectur/lib/presentation/splash/splash.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_mvvm_clean_architectur/presentation/resources/assets_manager.dart';
 import 'package:flutter_mvvm_clean_architectur/presentation/resources/color_manager.dart';
+import 'package:flutter_mvvm_clean_architectur/presentation/resources/routes_manager.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -10,11 +13,32 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  Timer? _timer;
+  _startDelay() {
+    _timer = Timer(Duration(seconds: 2), _goNext);
+  }
+
+  _goNext() {
+    Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startDelay();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.primary,
-      body: Center(child: Image(image: AssetImage(ImagesAssets.splashLogo)),),
+      body: Center(child: Image(image: AssetImage(ImagesAssets.splashLogo))),
     );
   }
 }
